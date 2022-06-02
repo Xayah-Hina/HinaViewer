3795a7f3b43486547ced47ccfd1f4173f68d7c04
Xayahp <ycxiang14sep@gmail.com>
2022-06-02 15:10:21 +0800

    clean dependencies
    Former-commit-id: 5fdb0aa72af96b1fd717f9c53e2b84e0882f3736

--- cmake/check_platform.cmake
+++ cmake/check_platform.cmake
@@ -1,5 +1,7 @@
 if (MSVC)
     include(E:/Git_Repos/vcpkg/scripts/buildsystems/vcpkg.cmake)
+    find_path(VCPKG_INCLUDE_DIR zlib.h)
+    include_directories(${VCPKG_INCLUDE_DIR})
 endif ()
 
 if (APPLE)
--- samples/sample4/main.cpp
+++ samples/sample4/main.cpp
@@ -1,11 +1,71 @@
-#include "HinaGUI/HinaViewer.h"
+//#include "HinaGUI/HinaViewer.h"
+
+#define _USE_MATH_DEFINES
+#include "OpenMesh/Core/IO/MeshIO.hh"
+#include "OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh"
+
+#include "GL/glut.h"
+#include <iostream>
+
+OpenMesh::TriMesh_ArrayKernelT<> mesh;
+
+void drawFunc() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glPushMatrix();
+    glBegin(GL_TRIANGLES);
+
+    for (OpenMesh::TriMesh_ArrayKernelT<>::FaceIter f_it = mesh.faces_begin(); f_it != mesh.faces_end(); ++f_it)
+    {
+        for (OpenMesh::TriMesh_ArrayKernelT<>::FVIter fv_it = mesh.fv_begin(*f_it); fv_it != mesh.fv_end(*f_it); ++fv_it)
+        {
+            glColor3ubv(mesh.color(*fv_it).data());
+            glVertex3fv(mesh.point(*fv_it).data());
+        }
+    }
+
+    glEnd();
+    glPopMatrix();
+
+    glFlush();
+}
 
 int main()
 {
-    HinaGUI::HinaViewer viewer;
-    viewer.init("C:/Users/Administrator/Desktop/BFU_Graphics/HinaGUI/HinaGUI/builtin-desc/classic_viewer.json");
-//    viewer.init("/Users/xayah/Desktop/BFU-Graphics/HinaGUI/HinaGUI/builtin-desc/classic_viewer.json");
-    viewer.launch();
-    viewer.kill();
+    mesh.request_vertex_colors();
+    OpenMesh::IO::Options opt;
+    opt += OpenMesh::IO::Options::VertexColor;
+    std::cout << OpenMesh::IO::read_mesh(mesh, "C:/Users/Administrator/Desktop/BFU_Graphics/HinaGUI/resources/bunny.off", opt) << std::endl;
+
+    int foo = 1;
+    char * bar[1] = {" "};
+    glutInit(&foo, bar);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+    glutInitWindowPosition(50, 50);
+    glutInitWindowSize(800, 800);
+    glutCreateWindow("first");
+
+    glShadeModel(GL_SMOOTH);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
+    glClearDepth(1.0f);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+    gluPerspective(80, 1, 0.1, 1000);
+    gluLookAt(0, 0, 500,	// eye
+              0, 0 , 0,			// center
+              0, 1, 0);			// Top
+
+    glutDisplayFunc(drawFunc);
+    //glutIdleFunc(drawFunc);
+    glutMainLoop();
+
+    system("pause");
+
+
+//    HinaGUI::HinaViewer viewer;
+//    viewer.init("C:/Users/Administrator/Desktop/BFU_Graphics/HinaGUI/HinaGUI/builtin-desc/classic_viewer.json");
+////    viewer.init("/Users/xayah/Desktop/BFU-Graphics/HinaGUI/HinaGUI/builtin-desc/classic_viewer.json");
+//    viewer.launch();
+//    viewer.kill();
     return 0;
 }
