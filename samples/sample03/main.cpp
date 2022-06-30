3e842ac4151626ef4b9a3adc6972eb5e62e415ff
Hina Xayah <ycxiang14sep@gmail.com>
2022-06-30 16:49:24 +0800

    support wireframe draw

--- samples/sample03/main.cpp
+++ samples/sample03/main.cpp
@@ -1,4 +1,6 @@
-#include "igl/readOBJ.h"
+#include "Eigen/Dense"
+#include "igl/read_triangle_mesh.h"
+#include "igl/edges.h"
 
 #include "main.h"
 
@@ -23,38 +25,97 @@ struct Vertex
 template<typename Vertex, typename Index>
 void Sample<Vertex, Index>::impl::init()
 {
-    std::vector<std::vector<float>> V;
-    std::vector<std::vector<int>> F;
-    igl::readOBJ("F:/Projects/HinaViewer/submodules/bgfx.cmake/bgfx/examples/assets/meshes/bunny.obj", V, F);
+    Eigen::MatrixXf V;
+    Eigen::MatrixXi F;
+    Eigen::MatrixXi E;
+//    igl::read_triangle_mesh("F:/Projects/HinaViewer/submodules/bgfx.cmake/bgfx/examples/assets/meshes/cube.obj", V, F);
+    igl::readOBJ("F:/Projects/HinaViewer/submodules/bgfx.cmake/bgfx/examples/assets/meshes/cube.obj", V, F);
+    igl::edges(F, E);
 
-    std::vector<Vertex> vertices;
-    for (const auto &i: V)
+    m_vertices.clear();
+    m_vertices.reserve(V.size());
+    for (int i = 0; i < V.rows(); ++i)
     {
         Vertex vertex{};
-        vertex.m_x = i[0];
-        vertex.m_y = i[1];
-        vertex.m_z = i[2];
+        vertex.m_x = V(i, 0);
+        vertex.m_y = V(i, 1);
+        vertex.m_z = V(i, 2);
         vertex.m_abgr = 0xff000000;
-        vertices.push_back(vertex);
+        m_vertices.push_back(vertex);
     }
     m_layout = Vertex::get_layout();
-    m_vbh = bgfx::createVertexBuffer(bgfx::makeRef(vertices.data(), sizeof(Vertex) * vertices.size()), m_layout);
+    m_vbh = bgfx::createVertexBuffer(bgfx::makeRef(&m_vertices[0], sizeof(Vertex) * m_vertices.size()), m_layout);
+
+    m_indices.clear();
+    m_indices.reserve(F.size());
+    for (int i = 0; i < F.rows(); ++i)
+        for (int j = 0; j < F.cols(); ++j)
+            m_indices.push_back(F(i, j));
+    m_ibh = bgfx::createIndexBuffer(bgfx::makeRef(m_indices.data(), sizeof(Index) * m_indices.size()));
+
+    m_edges.clear();
+    m_edges.reserve(E.size());
+    for (int i = 0; i < E.rows(); ++i)
+        for (int j = 0; j < E.cols(); ++j)
+            m_edges.push_back(E(i, j));
+    m_ebh = bgfx::createIndexBuffer(bgfx::makeRef(m_edges.data(), sizeof(Index) * m_edges.size()));
+    m_program = loadProgram("vs_sample01", "fs_sample01");
 }
 
 template<typename Vertex, typename Index>
 void Sample<Vertex, Index>::impl::update()
 {
+    const bx::Vec3 at = {0.0f, 0.0f, 0.0f};
+    const bx::Vec3 eye = {0.0f, 0.0f, -4.0f};
+    {
+        float view[16];
+        bx::mtxLookAt(view, eye, at);
 
+        float proj[16];
+        bx::mtxProj(proj, 60.0f, float(m_width) / float(m_height), 0.1f, 100.0f, bgfx::getCaps()->homogeneousDepth);
+        bgfx::setViewTransform(0, view, proj);
+
+        // Set view 0 default viewport.
+        bgfx::setViewRect(0, 0, 0, uint16_t(m_width), uint16_t(m_height));
+
+        float mtx[16];
+        bx::mtxIdentity(mtx);
+        bgfx::setTransform(mtx);
+    }
+
+    // This dummy draw call is here to make sure that view 0 is cleared
+    // if no other draw calls are submitted to view 0.
+    bgfx::touch(0);
+
+    uint64_t state = 0
+                     | BGFX_STATE_WRITE_R
+                     | BGFX_STATE_WRITE_G
+                     | BGFX_STATE_WRITE_B
+                     | BGFX_STATE_WRITE_A
+                     | BGFX_STATE_WRITE_Z
+                     | BGFX_STATE_DEPTH_TEST_LESS
+                     | BGFX_STATE_CULL_CW
+                     | BGFX_STATE_MSAA
+                     | BGFX_STATE_PT_LINES;
+
+    bgfx::setVertexBuffer(0, m_vbh);
+    bgfx::setIndexBuffer(m_ebh);
+    bgfx::setState(state);
+    bgfx::submit(0, m_program);
+
+    bgfx::frame();
 }
 
 template<typename Vertex, typename Index>
 void Sample<Vertex, Index>::impl::shutdown()
 {
-
+    bgfx::destroy(m_ibh);
+    bgfx::destroy(m_vbh);
+    bgfx::destroy(m_program);
 }
 
 int _main_(int _argc, char **_argv)
 {
-    Sample<Vertex, uint64_t> sample("03-samples", "Rendering simple static mesh.", "https://bkaradzic.github.io/bgfx/examples.html#cubes");
+    Sample<Vertex, uint16_t> sample("03-samples", "Rendering simple static mesh.", "https://bkaradzic.github.io/bgfx/examples.html#cubes");
     return runApp(&sample, _argc, _argv);
 }
--- samples/sample03/main.h
+++ samples/sample03/main.h
@@ -28,7 +28,7 @@ public:
 };
 
 template<typename Vertex, typename Index>
-Sample<Vertex, Index>::Sample(const char *name, const char *description, const char *url):AppI(name, description, url)
+Sample<Vertex, Index>::Sample(const char *name, const char *description, const char *url):AppI(name, description, url), m_impl(std::make_unique<Sample<Vertex, Index>::impl>())
 {}
 
 template<typename Vertex, typename Index>
@@ -39,11 +39,18 @@ template<typename Vertex, typename Index>
 struct Sample<Vertex, Index>::impl
 {
     uint32_t m_width{}, m_height{};
-    bgfx::VertexLayout m_layout;
+    uint32_t m_debug{}, m_reset{};
+    std::vector<Vertex> m_vertices;
+    std::vector<Index> m_indices;
+    std::vector<Index> m_edges;
 
+    bgfx::VertexLayout m_layout;
     bgfx::VertexBufferHandle m_vbh;
     bgfx::IndexBufferHandle m_ibh;
+    bgfx::IndexBufferHandle m_ebh;
     bgfx::ProgramHandle m_program;
+    entry::MouseState m_mouseState;
+
 
     void init();
     void update();
@@ -53,34 +60,64 @@ struct Sample<Vertex, Index>::impl
 template<typename Vertex, typename Index>
 void Sample<Vertex, Index>::init(int32_t _argc, const char *const *_argv, uint32_t _width, uint32_t _height)
 {
+    m_impl->m_width = _width;
+    m_impl->m_height = _height;
+    m_impl->m_debug = BGFX_DEBUG_NONE;
+    m_impl->m_reset = BGFX_RESET_VSYNC;
+
     Args args(_argc, _argv);
     bgfx::Init init;
     init.type = args.m_type;
     init.vendorId = args.m_pciId;
     init.resolution.width = _width;
     init.resolution.height = _height;
-    init.resolution.reset = BGFX_RESET_VSYNC;
+    init.resolution.reset = m_impl->m_reset;
     bgfx::init(init);
-    bgfx::setDebug(BGFX_DEBUG_NONE);
-    bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x000000ff, 1.0f, 0);
-
-    m_impl->m_width = _width;
-    m_impl->m_height = _height;
+    bgfx::setDebug(m_impl->m_debug);
+    bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0xffffffff, 1.0f, 0);
 
     m_impl->init();
+
+    imguiCreate();
 }
 
 template<typename Vertex, typename Index>
 bool Sample<Vertex, Index>::update()
 {
-    m_impl->update();
-    return true;
+    if (!entry::processEvents(m_impl->m_width, m_impl->m_height, m_impl->m_debug, m_impl->m_reset, &m_impl->m_mouseState))
+    {
+        imguiBeginFrame(m_impl->m_mouseState.m_mx, m_impl->m_mouseState.m_my, (m_impl->m_mouseState.m_buttons[entry::MouseButton::Left] ? IMGUI_MBUT_LEFT : 0)
+                                                              | (m_impl->m_mouseState.m_buttons[entry::MouseButton::Right] ? IMGUI_MBUT_RIGHT : 0)
+                                                              | (m_impl->m_mouseState.m_buttons[entry::MouseButton::Middle] ? IMGUI_MBUT_MIDDLE : 0), m_impl->m_mouseState.m_mz, uint16_t(m_impl->m_width), uint16_t(m_impl->m_height)
+        );
+
+        showExampleDialog(this);
+
+        ImGui::SetNextWindowPos(
+                ImVec2(m_impl->m_width - m_impl->m_width / 5.0f - 10.0f, 10.0f), ImGuiCond_FirstUseEver
+        );
+        ImGui::SetNextWindowSize(
+                ImVec2(m_impl->m_width / 5.0f, m_impl->m_height / 3.5f), ImGuiCond_FirstUseEver
+        );
+        ImGui::Begin("Settings", NULL, 0);
+
+        ImGui::End();
+
+        m_impl->update();
+
+        imguiEndFrame();
+
+        return true;
+    }
+    return false;
 }
 
 template<typename Vertex, typename Index>
 int Sample<Vertex, Index>::shutdown()
 {
+    imguiDestroy();
     m_impl->shutdown();
+    bgfx::shutdown();
     return 0;
 }
 
